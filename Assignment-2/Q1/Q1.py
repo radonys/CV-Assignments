36ec4425d9d5ddd022e20074e34c23fa2bc7c51e
Vaishnav Murali <vaishnavmurali@gmail.com>
2018-03-12 17:42:23 +0530

    K means clustering pickle function
    Signed-off-by: Vaishnav Murali <vaishnavmurali@gmail.com>

--- Assignment-2/Q1/Q1.py
+++ Assignment-2/Q1/Q1.py
@@ -1,6 +1,8 @@
 import numpy as np
 import os
 import heapq
+import pickle
+from sklearn.cluster import KMeans
 def Edistance(v1,v2):
     return np.sum(np.subtract(v1,v2)**2)
 N = 9
@@ -24,14 +26,14 @@ class cus_heap():
 acc = 0
 count = 0
 heap = cus_heap()
-for j in os.listdir("../Data/test_sift_features"):
-    for i in os.listdir("../Data/train_sift_features"):
-        f1 = np.loadtxt(open("../Data/train_sift_features/"+i),delimiter=',')
-        f2 = np.loadtxt(open("../Data/test_sift_features/"+j),delimiter=',')
-        print(f1.shape,f2.shape)
-        heap.push((Edistance(f1.flatten(),f2.flatten()),int(i.split("_")[0])-1))
-    res = [labels_training(i[1]) for i in heap.nsmallest(N)]
-    if max(set(res),key=res.count)==labels_testing[int(j.split("_")[0])]:
-        print("match: {}".format(labels_testing[int(j.split("_")[0])]))
-        acc+=1
+main_data = np.array([])
+for i in os.listdir("../Data/train_sift_features"):
+    if len(main_data):
+        main_data = np.vstack((main_data,np.loadtxt(open("../Data/train_sift_features/"+i),delimiter=",")))
+    else:
+        main_data = np.loadtxt(open("../Data/train_sift_features/"+i),delimiter=",")
+print(main_data.shape)
+t = KMeans(n_clusters=50).fit(main_data)
+pickle.dump(t,open("k-means.pkl","wb"))
+print(t.cluster_centers_)
 print("acc:{}",acc/len(labels_testing))
